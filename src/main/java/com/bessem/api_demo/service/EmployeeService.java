5a9a6cf28e4ec4715fc9525b355221082bcc17b8
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-06-17 17:23:01 +0200

    add unit test

--- src/main/java/com/bessem/api_demo/service/EmployeeService.java
+++ src/main/java/com/bessem/api_demo/service/EmployeeService.java
@@ -1,5 +1,37 @@
 package com.bessem.api_demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bessem.api_demo.model.Employee;
+import com.bessem.api_demo.repository.EmployeeRepository;
+
+import lombok.Data;
+
+@Data
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Optional<Employee> getEmployee(final Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Iterable<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(final Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return savedEmployee;
+    }
 
 }
